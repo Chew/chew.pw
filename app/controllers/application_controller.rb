2faff08ee507ec98bc74b9d461ac493acdad1649
Chew <chew@chew.pw>
2021-03-24 00:52:48 -0500

    Global application layout (convert all pages to Bootstrap 5)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :set_raven_context
+  before_action :info
+
+  def info
+    @controller_name = request.controller_class.to_s.gsub("Controller", "").downcase
+
+    @layout = Dir['app/views/layouts/extra/*'].map { |f| File.basename(f, '.html.erb') }.include? "_#{@controller_name}"
+    @nav = Dir['app/views/layouts/navs/*'].map { |f| File.basename(f, '.html.erb') }.include? "_#{@controller_name}"
+  end
 
   private
 
