34a78edea50066b714bcbec4256d509c5e0e8e28
Chew <chew@chew.pw>
2021-09-29 22:14:25 -0500

    use permissions and content security policies

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -1,11 +1,14 @@
 # Define an application-wide HTTP permissions policy. For further
 # information see https://developers.google.com/web/updates/2018/06/feature-policy
-#
-# Rails.application.config.permissions_policy do |f|
-#   f.camera      :none
-#   f.gyroscope   :none
-#   f.microphone  :none
-#   f.usb         :none
-#   f.fullscreen  :self
-#   f.payment     :self, "https://secure.example.com"
-# end
+
+Rails.application.config.permissions_policy do |f|
+  f.camera       :none
+  f.gyroscope    :none
+  f.microphone   :none
+  f.usb          :none
+  f.autoplay     :none
+  f.magnetometer :none
+  f.midi         :none
+  f.fullscreen   :self
+  f.payment      :self
+end
