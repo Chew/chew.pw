9f6fefc3fb1f5ccb0ef487adc78a9a7711eeb511
Chew <chew@chew.pw>
2022-09-03 16:53:33 -0500

    Clean up home page for NFL and add it back to navbar

--- app/controllers/sports_controller.rb
+++ app/controllers/sports_controller.rb
@@ -18,14 +18,6 @@ class SportsController < ApplicationController
     @season = params[:season]
   end
 
-  # @return [Nokogiri::HTML::Document]
-  def retrieve_nfl_stats(year = 2021, season = "REG")
-    data = Rails.cache.fetch('sports-nfl', expires_in: 1.day) do
-      RestClient.get('https://www.nfl.com/standings/league/2021/REG', 'User-Agent': DUMMY_USER_AGENT).body
-    end
-    Nokogiri.parse(data)
-  end
-
   NFL_USER_AGENT = "NFLMobile/14471 CFNetwork/1390 Darwin/22.0.0"
 
   # Cycles the NFL token.
@@ -68,68 +60,7 @@ class SportsController < ApplicationController
   end
 
   def nfl
-    stats = retrieve_nfl_stats
-
-    # get the table tag
-    table = stats.search('table')[0]
-
-    if table.nil?
-      render json: { error: "Could not find table" }
-      return
-    end
-
-    # Get the headers from the table head
-    headers = table.search('thead tr th').map { |e| e.text.strip }
-
-    # Get the teams from the table body
-    teams = table.search('tbody tr').map { |e| e.search('td').map {|f| f.text.strip } }
-
-    @headers = headers
-    @teams = teams
-  end
-
-  def nfl_api
-    stats = retrieve_nfl_stats
-
-    # get the table tag
-    table = stats.search('table')[0]
-
-    if table.nil?
-      json_error_response("Could not find table")
-      return
-    end
-
-    # Get the headers from the table head
-    headers = table.search('thead tr th').map { |e| e.text.strip }
-
-    # Get the teams from the table body
-    teams = table.search('tbody tr').map { |e| e.search('td').map {|f| f.text.strip } }
-
-    table = {}
-
-    cur_team = ""
-    teams.each do |team|
-      team.each_with_index do |data, i|
-        if i == 0
-          # @type [Array<String>]
-          name_data = data.split("\n")
-          while name_data.last.strip.length <= 3
-            name_data.pop
-          end
-
-          short = name_data.last.strip
-          full_name = name_data.first
-          cur_team = short
-          table[short] = {}
-          table[short]["name"] = full_name
-          next
-        end
-
-        table[cur_team][headers[i]] = data
-      end
-    end
-
-    json_response table
+    @teams = nfl_web_request("https://api.nfl.com/football/v1/standings?season=2022&seasonType=REG")['weeks']
   end
 
   def nfl_game
