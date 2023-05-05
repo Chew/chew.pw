1d0391a4b223274c789793138f62135b26aa0ea3
Chew <chew@chew.pw>
2023-05-05 13:57:21 -0400

    Fix solitaire daily challenge data

--- app/controllers/solitaire_controller.rb
+++ app/controllers/solitaire_controller.rb
@@ -8,12 +8,25 @@ class SolitaireController < ApplicationController
   def challenges
     date = params['month'] + params['year'].split('')[2..4].join('')
 
-    @info = Rails.cache.fetch("ms-solitaire-challenges-#{date}") do
-      begin
-        JSON.parse RestClient.get("https://download-ssl.msgamestudios.com/content/mgs/ce/production/SolitaireWin10/prod/MonthChallenges#{date}.js/1/MonthChallenges#{date}.js")
-      rescue RestClient::NotFound
-        flash[:modal_js] = "No challenge data found for this month!"
-        return redirect_to "/solitaire/challenges/01/2015"
+    @archive = Rails.cache.fetch("ms-solitaire-challenges-archive", expires_in: 1.day) do
+      JSON.parse(RestClient.get("https://download-ssl.msgamestudios.com/content/mgs/ce/production/Solitaire/prod/dailychallenges/archive_manifest_c4ff6b287ca51e576d604a00a563f196732fc186.json"))['categories'][0]['entries']
+    end
+
+    @info = Rails.cache.fetch("ms-solitaire-challenges-#{date}", expires_in: 1.day) do
+      # Find key
+      name = nil
+      @archive.each do |entry|
+        # "key": "monthchallenges0119"
+        next unless entry['key'] == "monthchallenges#{date}"
+
+        name = entry['name']
+      end
+
+      if name
+        JSON.parse RestClient.get("https://download-ssl.msgamestudios.com/content/mgs/ce/production/Solitaire/prod/dailychallenges/#{name}")
+      else
+        flash[:modal_js] = "No challenge data found for that month!"
+        return redirect_to "/solitaire/challenges"
       end
     end
   end
