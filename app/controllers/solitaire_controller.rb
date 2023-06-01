228c1bec2230c682b1b033a8a0baf7e305d683e6
Chew <chew@chew.pw>
2023-06-01 00:05:30 -0500

    Updated solitaire months and clean up design

--- app/controllers/solitaire_controller.rb
+++ app/controllers/solitaire_controller.rb
@@ -3,13 +3,18 @@ class SolitaireController < ApplicationController
   end
 
   def challenges_list
+    @archive = Rails.cache.fetch("ms-solitaire-challenges-archive", expires_in: 1.day) do
+      JSON.parse(RestClient.get("https://go.microsoft.com/fwlink/?linkid=2181471"))['categories'][0]['entries']
+    end
+
+    @months = @archive.map {|e| e['key']}
   end
 
   def challenges
     date = params['month'] + params['year'].split('')[2..4].join('')
 
     @archive = Rails.cache.fetch("ms-solitaire-challenges-archive", expires_in: 1.day) do
-      JSON.parse(RestClient.get("https://download-ssl.msgamestudios.com/content/mgs/ce/production/Solitaire/prod/dailychallenges/archive_manifest_c4ff6b287ca51e576d604a00a563f196732fc186.json"))['categories'][0]['entries']
+      JSON.parse(RestClient.get("https://go.microsoft.com/fwlink/?linkid=2181471"))['categories'][0]['entries']
     end
 
     @info = Rails.cache.fetch("ms-solitaire-challenges-#{date}", expires_in: 1.day) do
