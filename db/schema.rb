264ef31d018c244a1ea0cc49eafe899111f1e6e3
Chew <chew@chew.pw>
2023-06-22 16:52:26 -0500

    update new DB credentials and re-dump schema

--- db/schema.rb
+++ db/schema.rb
@@ -26,39 +26,19 @@ ActiveRecord::Schema.define(version: 2020_12_05_194150) do
     t.index ["command"], name: "command", unique: true
   end
 
-  create_table "chewbotcca_profiles", primary_key: "userid", id: :bigint, default: nil, charset: "utf8", force: :cascade do |t|
+  create_table "chewbotcca_profiles", primary_key: "userid", id: { type: :string, limit: 20 }, charset: "utf8", force: :cascade do |t|
     t.text "lastfm"
     t.text "github"
   end
 
   create_table "chewbotcca_server_emojis", id: false, charset: "utf8", force: :cascade do |t|
     t.bigint "id", null: false
-    t.json "emojis", null: false
+    t.text "emojis", size: :long, null: false, collation: "utf8mb4_bin"
   end
 
   create_table "chewbotcca_servers", primary_key: "serverid", id: :bigint, default: nil, charset: "utf8", force: :cascade do |t|
     t.text "prefix"
     t.boolean "bypass_urban_nsfw", default: false, null: false
-  end
-
-  create_table "hq_questions", id: :integer, charset: "utf8", force: :cascade do |t|
-    t.text "region", null: false
-    t.text "question", null: false
-    t.text "option1", null: false
-    t.text "option2", null: false
-    t.text "option3", null: false
-    t.integer "correct", null: false
-    t.integer "ques_num", null: false
-    t.text "time", null: false
-    t.text "prize", null: false
-    t.integer "picked1", null: false
-    t.integer "picked2", null: false
-    t.integer "picked3", null: false
-    t.integer "winners", null: false
-    t.text "payout", null: false
-    t.text "currency", null: false
-    t.text "category", null: false
-    t.text "category_slug", null: false
   end
 
   create_table "hqtriviabot_logins", primary_key: "userid", id: :bigint, default: nil, charset: "utf8", force: :cascade do |t|
@@ -78,12 +58,6 @@ ActiveRecord::Schema.define(version: 2020_12_05_194150) do
     t.boolean "superspins", default: false, null: false
     t.boolean "coins", default: false, null: false
     t.boolean "donator", default: false, null: false
-  end
-
-  create_table "mcpro_faq", id: :integer, charset: "utf8", force: :cascade do |t|
-    t.text "data", size: :long, null: false
-    t.boolean "hidden", default: false, null: false
-    t.timestamp "last_updated", default: -> { "CURRENT_TIMESTAMP" }, null: false
   end
 
 end
